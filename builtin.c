a7c7a703331239f4691fa87508ea1c19f160fcea
Abduls4u <abdulsalamabdulsomad14@gmail>
2022-11-16 00:36:49 -0800

    fixed all errors

--- builtin.c
+++ builtin.c
@@ -53,7 +53,7 @@ int _mycd(info_t *info)
 			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
-{
+	{
 		if (!_getenv(info, "OLDPWD="))
 		{
 			_puts(s);
@@ -76,22 +76,5 @@ int _mycd(info_t *info)
 		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
 		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
-	return (0);
-
-
-/**
- * _myhelp - changes the current directory of the process
-* @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
-int _myhelp(info_t *info)
-{
-	char **arg_array;
-
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
--- getenv.c
+++ getenv.c
@@ -32,8 +32,7 @@ int _unsetenv(info_t *info, char *var)
 
 	if (!node || !var)
 		return (0);
-
-while (node)
+	while (node)
 	{
 		p = starts_with(node->str, var);
 		if (p && *p == '=')
